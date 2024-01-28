7b3edf6bc15dcabad0c6f37774e1d66fc10220b2
Adenike <adeyinkaadenike992@gmail.com>
2024-01-29 00:30:31 +0100

    commitd today

---
+++ src/Color/Colors.js
@@ -0,0 +1,14 @@
+// colors.js
+
+export const Colors = {
+    primary: '#108EE9',
+    blue: '#1677FF',
+    orange :'#FD9A16',
+    darkRed: '#EA4E0D',
+    Red: '#F95654',
+    yellow: '#FDD504',
+    red : "#F95654",
+    black: "#242424",
+    white : "#FFFFFF"
+   
+};
